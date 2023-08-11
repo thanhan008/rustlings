4889fee158da938946cd30b8f91a9ed11ba13638
Tran Thanh An <tranthanhan0903@gmail.com>
2023-08-11 15:26:20 +0700

    modify exercises/variables/variables5.rs

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -2,10 +2,12 @@
 //
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
-
+// IM NOT DONE
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    let number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    {
+        let number = 3; // don't rename this variable
+        println!("Number plus two is : {}", number + 2);
+    }
 }
